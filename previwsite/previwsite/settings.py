1e4b4031eb57c4f1cec4f4401c76532b7f5f1fdf
odiinnn <38395645+odiinnn@users.noreply.github.com>
2021-10-12 01:14:07 +0300

    Update settings.py

--- previwsite/previwsite/settings.py
+++ previwsite/previwsite/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$&hjg9+d=735q496un5#-0_o=vj3%0f4wa9u&96n+q^wlx0puj'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
